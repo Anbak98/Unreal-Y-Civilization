fb71acf2418688f728b3441d5cd8d201ee7c2c68
anbak98 <anbak98@gmail.com>
2024-09-04 15:55:35 +0900

    Init: Unreal Project File

---
+++ YCivilization/Source/YCivilization/YCivilization.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "YCivilization.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, YCivilization, "YCivilization" );
+
+DEFINE_LOG_CATEGORY(LogYCivilization)
+ 
